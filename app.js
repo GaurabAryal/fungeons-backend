a5ce635a5b20002a0f3ced09b0ac4deb3a7f5bce
Gaurab Aryal <garaub.aryal@qlik.com>
2016-01-22 16:40:15 -0500

    removed readme

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var bodyparser = require('body-parser');
-var http = require('http');
-var mongodb = require('mongodb');
-var dbConfig = require('./db.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var app = express();
-mongoose.connect(dbConfig.url);
