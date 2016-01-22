b83742abc3575c917c176c8fba9c43211a5d3d5b
Gaurab Aryal <garaub.aryal@qlik.com>
2016-01-22 16:40:15 -0500

    Done hacking for the duration

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-
+//https://github.com/tutsplus/passport-mongo/blob/master/app.js
+//https://medium.com/@aleicher/should-i-use-heroku-or-aws-3bfcd4706a36#.34ly93yj3
 module.exports = mongoose.model('User',{
     username: String,
     password: String,
