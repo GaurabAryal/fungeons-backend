b49b7cb4f9878ed81bffc33d2f79abc86e4eb90e
Gaurab Aryal <gaurab.aryal7@gmail.com>
2016-03-12 15:10:51 -0500

    stuff

--- controllers/users.js
+++ controllers/users.js
@@ -40,6 +40,11 @@ module.exports = function(app, io){
     }
   ));
 
+  app.post('/', function(req, res){
+    console.log("hiii");
+
+  });
+
   app.post('/register', function(req, res){
     console.log(req.body);
     var username = req.body.username, password = req.body.password;
