08b830b67a969e1150b51b215e44a5a24030b678
Gaurab Aryal <gaurab.aryal7@gmail.com>
2016-04-17 02:57:23 -0400

    SocketIO Fix - MAYBE

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ server.listen(config.serverport, config.serverip, function() {
 mongoose.connect(config.mongodb);
 app.use(bodyParser());
 //ONLY use redis when deployed.
-// if (config.redis_host){
-//   console.log("Here!");
-//   io.adapter(redis({ host: config.redis_host, port: config.redis_port, auth_pass: process.env.OPENSHIFT_REDIS_DB_PASSWORD}));
-// }
+
 var users = require("./controllers/users")(app, io);
 
 var apiRoutes = express.Router();
@@ -69,14 +66,3 @@ var gamerooms = require("./controllers/gamerooms")(apiRoutes, io);
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
 module.exports = app;
-// app.get('/', function (req, res) {
-//   res.json("Hii!!");
-// });
-// io.on('connection', function (socket) {
-//   socket.emit('hello', { hello: 'Hi' });
-//
-//   socket.on('hi', function (data) {
-//     console.log("received hi from client: ", data);
-//     socket.emit('sup', { sup: 'What\'s Up?' });
-//   });
-// });
